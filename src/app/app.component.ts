d407838654f14d4d4195a6104c24c97fc1a56242
Halder <681019@cognizant.com>
2018-03-29 18:01:42 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-cts',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Study';
+  name='deboleena'
+  num1=15;
+  num2=13;
+  displayDate=function()
+  {
+    return new Date();
+  }
 }
