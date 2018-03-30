4d87d2836735cc366d7339e54cbf97bbff4c9740
Halder <681019@cognizant.com>
2018-03-30 16:10:20 +0530

    angularcomponentandrouter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { FamilyComponent } from './family/family.component';
+import { ProductsComponent } from './shop/products/products.component';
+import { CatalogComponent } from './shop/catalog/catalog.component';
+import{RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FamilyComponent,
+    ProductsComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule
     FormsModule
+    RouterModule.forRoot([
+      {
+        path:"catalog",
+        component:CatalogComponent 
+
+    }
+    {
+path:"products",
+component:ProductsComponent
+
+
+    }
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
